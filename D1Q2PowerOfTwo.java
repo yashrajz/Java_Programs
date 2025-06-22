979b84e5047b3e233214d157f878457d5bd4b328
yashrajz <yashrajz.me@gmail.com>
2025-06-22 12:45:11 +0530

    Add D1Q2PowerOfTwo class to determine if a number is a power of two

---
+++ D1Q2PowerOfTwo.java
@@ -0,0 +1,25 @@
+public class D1Q2PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n < 1){
+            return false;
+        }
+        else if(n == 1){
+            return true;
+        }
+        else{
+
+            while(n % 2 == 0){
+                n /= 2;
+            }
+
+            if(n == 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+
+    }
+}
