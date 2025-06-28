a501cd5845d2e0750b6fba34bc6173c41a05b8f3
yashrajz <yashrajz.me@gmail.com>
2025-06-28 16:12:10 +0530

    Add D7Q1TwoSumII-InputArrayIsSorted class to implement the two-sum problem for a sorted array

---
+++ D7Q1TwoSumII-InputArrayIsSorted.java
@@ -0,0 +1,26 @@
+// Leetcode question - 167
+public class D7Q1TwoSumII-InputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while(start < end){
+            int sum = numbers[start] + numbers[end];
+
+            if(sum == target){
+                ans[0] = start + 1;
+                ans[1] = end + 1;
+                return ans;
+            }
+            else if(sum < target){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+        return ans;
+    }
+}
