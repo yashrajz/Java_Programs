edb3c3fdf177e0fcb6cca334b78054ebaa1ba264
yashrajz <yashrajz.me@gmail.com>
2025-07-01 13:02:43 +0530

    Add D10Q1FindLuckyIntegerInAnArray class to implement the lucky integer in an array problem

---
+++ D10Q1FindLuckyIntegerInAnArray.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+Leetcode question - 1394
+public class D10Q1FindLuckyIntegerInAnArray {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int num : arr) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+
+        int result = -1;
+
+        for (int num : hm.keySet()) {
+            if (num == hm.get(num)) {
+                result = Math.max(result, num);
+            }
+        }
+
+        return result;
+    }
+}
