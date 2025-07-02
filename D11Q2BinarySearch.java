52692e7198dd3f3bc2922c711757594c4c014a12
yashrajz <yashrajz.me@gmail.com>
2025-07-02 13:11:04 +0530

    Add D11Q2BinarySearch class to implement binary search algorithm

---
+++ D11Q2BinarySearch.java
@@ -0,0 +1,23 @@
+// Leetcode questions - 34
+public class D11Q2BinarySearch {
+    public int search(int[] nums, int target) {
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
